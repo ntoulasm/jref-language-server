f3e2d751ef48cd93c3a159a62464fca39f765416
Marios Ntoulas <ntoulasm@csd.uoc.gr>
2026-03-01 09:34:48 +0200

    feat: traverse ast to spot references
    - Added a recursive visitor to traverse the AST.
    - Added logic to identify `$ref`s.

--- server/src/server.ts
+++ server/src/server.ts
@@ -121,8 +121,52 @@ documents.onDidClose((e) => {
 documents.onDidChangeContent((change: TextDocumentChangeEvent<TextDocument>) => {
   const errors: ParseError[] = [];
   const ast: Node | undefined = parseTree(change.document.getText(), errors);
+  visit(ast);
   sendDiagnostics(change.document, errors);
 });
+
+function isCompositeNode(node: Node): boolean {
+  return node.type === 'object' || node.type === 'array';
+}
+
+function visitCompositeNode(node: Node) {
+  const children = node?.children || [];
+  for (const child of children) {
+    visit(child);
+  }
+}
+
+const visitFunctions: Record<string, (node: Node) => void> = {
+  object: visitCompositeNode,
+  array: visitCompositeNode,
+  property: (node: Node) => {
+    const children = node?.children || [];
+    if (children.length !== 2) {
+      /* incomplete property */
+      return;
+    }
+    const key = children[0];
+    const value = children[1];
+    const isReference = key.type === 'string' && key.value === '$ref' && value.type === 'string';
+    if (isReference) {
+      const ref = value.value;
+      console.log('Reference found: ' + ref);
+    }
+
+    if (isCompositeNode(value)) {
+      visit(value);
+    }
+  },
+};
+
+function visit(node: Node | undefined) {
+  if (!node?.type) {
+    console.error('Node type is undefined');
+    return;
+  }
+  const visit = visitFunctions[node.type];
+  visit(node);
+}
 
 function sendDiagnostics(document: TextDocument, parseErrors: ParseError[]) {
   connection.sendDiagnostics({
